d84a7a55a4d43ab96111722da92d91fac981a0c6
likhithkp <likhithkpdev@gmail.com>
2024-12-08 19:59:48 +0530

    Recurson started

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log();
+//Recursion
+
+//Factorial of n
+function factorial(num){
+    if(num === 0){
+        return 1;
+    } else {
+        return num * factorial(num - 1);
+    }
+}
+
+// Create an array with range of numbers
+function createArray(startNum, endNum){
+    if(endNum < startNum){
+        return [];
+    } else {
+        let array = createArray(startNum, endNum - 1);
+        array.push(endNum);
+        return array;
+    }
+}
+
+// Given and integer x, return true if x is palindrome, and false otherwise.
